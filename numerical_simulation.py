d86bc13fa1c43ebeca855757f22539fc32e84a5e
1024xx4 <teraleaf1024.4@gmail.com>
2021-11-27 14:37:41 +0900

    Practice Knock71: 人間関係の Network を可視化してみよう

---
+++ numerical_simulation.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     format: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
